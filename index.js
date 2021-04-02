ffc1b29fd71cea10a400ebc13ee8302ddb5a7cb9
Phil <p.mcdonald.jr@gmail.com>
2021-04-01 22:02:10 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+const copyOfDrivers = [...drivers]
+
+function destructivelyAppendDriver(name) {
+    drivers.push("Ralph");
+}
+
+function destructivelyPrependDriver(name){
+    drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop("Ralph");
+}
+
+function destructivelyRemoveFirstDriver(name) {
+    drivers.shift("Bob");
+}
+
+function appendDriver(name) {
+    return [...drivers, name];
+}
+
+function prependDriver(name) {
+    return [name, ...drivers];
+}
+
+function removeLastDriver(){
+    return drivers.slice(0,2);    
+}
+
+function removeFirstDriver() {
+    return drivers.slice(1)
+}
+
+
